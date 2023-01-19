6a6cf26d1765da62f02df2f68af13b66bcb850e7
thesudeshdas <sudeshkumardas7@gmail.com>
2023-01-19 21:53:05 +0530

    res: portfolio nav

--- src/components/Navs/PortfolioNav.tsx
+++ src/components/Navs/PortfolioNav.tsx
@@ -1,4 +1,11 @@
-import { Button, Divider, Flex, Heading, Stack } from '@chakra-ui/react';
+import {
+  Button,
+  Center,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+} from '@chakra-ui/react';
 import { Link, useLocation } from 'react-router-dom';
 
 export type IPortfolioNavItem = { text: string; matcher: string; url: string };
@@ -47,30 +54,44 @@ export default function PortfolioNav({ userId }) {
   };
 
   return (
-    <Flex gap={12}>
-      {portfolioNav.items.map((item) => {
-        return location.pathname === item.matcher ? (
-          <Stack key={item.text}>
-            <Link to={item.url}>
+    <Center>
+      <Flex
+        gap={{ base: 0, md: 8, lg: 12 }}
+        w={{ base: '100%', md: '80%', lg: '100%' }}
+        justifyContent={{ base: 'space-around', md: 'flex-start' }}
+      >
+        {portfolioNav.items.map((item) => {
+          return location.pathname === item.matcher ? (
+            <Stack key={item.text}>
+              <Link to={item.url}>
+                <Button variant='ghost'>
+                  <Heading
+                    size={{ base: 'sm', lg: 'md' }}
+                    fontWeight='600'
+                    color='black'
+                  >
+                    {item.text}
+                  </Heading>
+                </Button>
+              </Link>
+
+              <Divider height='4px' bg='black' opacity={1} borderRadius='md' />
+            </Stack>
+          ) : (
+            <Link key={item.text} to={item.url}>
               <Button variant='ghost'>
-                <Heading size='md' fontWeight='600' color='black'>
+                <Heading
+                  size={{ base: 'sm', lg: 'md' }}
+                  fontWeight='600'
+                  color='gray'
+                >
                   {item.text}
                 </Heading>
               </Button>
             </Link>
-
-            <Divider height='4px' bg='black' opacity={1} borderRadius='md' />
-          </Stack>
-        ) : (
-          <Link key={item.text} to={item.url}>
-            <Button variant='ghost'>
-              <Heading size='md' fontWeight='600' color='gray'>
-                {item.text}
-              </Heading>
-            </Button>
-          </Link>
-        );
-      })}
-    </Flex>
+          );
+        })}
+      </Flex>
+    </Center>
   );
 }
